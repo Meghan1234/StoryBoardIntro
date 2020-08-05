b7efb2d2a70cc9c7d156d8b550a3c1ab0f315f92
Meghan1234 <51449030+Meghan1234@users.noreply.github.com>
2020-08-05 08:37:56 -0700

    Add files via upload

---
+++ StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Meghan Jachna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement!")
+    }
+
+
+}
+
